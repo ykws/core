f0551c021e6bd015c8fc144e277e71ccbbe14784
Syoichi Tsuyuhara <syoichi@outlook.com>
2014-12-17 12:52:16 +0900

    Fix notify.ICON_INFO

--- xpi/chrome/content/library/utility.js
+++ xpi/chrome/content/library/utility.js
@@ -117,7 +117,7 @@ function notify(title, msg, icon){
 		icon, title, msg,
 		false, '', null);
 }
-notify.ICON_INFO = 'chrome://global/skin/console/bullet-question.png';
+notify.ICON_INFO = 'chrome://global/skin/icons/information-64.png';
 
 /**
  * URIを生成する。
