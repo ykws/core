70b87995a46c46958f6df3ebd8afc0f6368b7af3
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-01-21 17:09:58 +0900

    Remove unnecessary some spaces in overlay/

--- xpi/chrome/content/overlay/overlay.js
+++ xpi/chrome/content/overlay/overlay.js
@@ -2,7 +2,7 @@
   // createInstanceで呼び出し初期化を促す
   // 他の場所ではgetServiceを用いてよい
   var env = Cc['@tombfix.github.io/tombfix-service;1'].createInstance().wrappedJSObject;
-  
+
   window.addEventListener('load', function(e){
     // Element Hiding Helper for Adblock Plusとの衝突を避ける
     // (詳細不明/ロード時の処理を遅延し起動表示を高速化する)
