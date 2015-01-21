5d793c0475b964f501039e31dd7df5e6e8d4bbed
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-01-21 17:17:26 +0900

    Revise overlay.js #12
    * Gruntfile.js: Uncomment overlay.js

--- xpi/chrome/content/overlay/overlay.js
+++ xpi/chrome/content/overlay/overlay.js
@@ -1,13 +1,19 @@
-(function(){
+/* global Components */
+
+(function executeOverlay(win) {
+  'use strict';
+
   // createInstanceで呼び出し初期化を促す
   // 他の場所ではgetServiceを用いてよい
-  var env = Cc['@tombfix.github.io/tombfix-service;1'].createInstance().wrappedJSObject;
+  let env = Components.classes[
+    '@tombfix.github.io/tombfix-service;1'
+  ].createInstance().wrappedJSObject;
 
-  window.addEventListener('load', function(e){
+  win.addEventListener('load', evt => {
     // Element Hiding Helper for Adblock Plusとの衝突を避ける
     // (詳細不明/ロード時の処理を遅延し起動表示を高速化する)
-    setTimeout(function(){
-      env.signal(env, 'browser-load', e);
+    setTimeout(() => {
+      env.signal(env, 'browser-load', evt);
     }, 0);
-  }, false);
-})();
+  });
+}(window));
