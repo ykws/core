00814fc8a4818d2c3322dcc4b0557df6c7b2068f
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-02-25 14:11:27 +0900

    repository.js: Remove unnecessary lines

--- xpi/chrome/content/library/repository.js
+++ xpi/chrome/content/library/repository.js
@@ -6,36 +6,36 @@ Repository.prototype = {
     get size(){
         return this.names.length;
     },
-    
+
     get names(){
         return this.values.map(itemgetter('name'));
     },
-    
+
     get values(){
         return map(itemgetter(1), this).filter(function(v){
             return v.name;
         });
     },
-    
+
     clear : function(){
         this.names.forEach(function(name){
             delete this[name];
         }, this);
     },
-    
+
     find : function(name){
         return this.values.filter(function(i){
             return i.name && i.name.search(name) != -1;
         });
     },
-    
+
     copyTo : function(t){
         forEach(this, function(m){
             t[m[0]] = m[1];
         });
         return t;
     },
-    
+
     check : function(){
         var args = arguments;
         return reduce(function(memo, i){
@@ -44,7 +44,7 @@ Repository.prototype = {
             return memo;
         }, this.values, []);
     },
-    
+
     /**
      * 新しい定義を追加する。
      *
@@ -55,20 +55,20 @@ Repository.prototype = {
     register : function(defs, target, after){
         if(!defs)
             return;
-        
+
         defs = [].concat(defs);
         if(target){
             var vals = this.values;
             this.clear();
-            
+
             for(var i=0 ; i < vals.length ; i++)
                 if(vals[i].name == target)
                     break;
-            
+
             vals.splice.apply(vals, [(after? i+1 : i), 0].concat(defs));
             defs = vals;
         }
-        
+
         defs.forEach(function(d){
             this[d.name] = d;
         }, this);
