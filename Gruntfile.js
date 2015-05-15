ee26341d1c12cf472d7d349f2f2410f6fdf73f76
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-05-15 18:20:30 +0900

    Check syntax and coding style by JSCS, other than Gruntfile.js
    * Gruntfile.js: Refine jscs task
     * Rename "all" to "node" in jscs
     * Add "tombfix" & "main" & "old" & "prefs" in jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,15 +29,119 @@ module.exports = function createGruntConfig(grunt) {
       }
     },
     jscs: {
-      all: {
+      node: {
         files: {
-          src: [
-            'Gruntfile.js'
-          ]
+          src: 'Gruntfile.js'
         },
         options: {
           config: '.jscsrc',
           esnext: null
+        }
+      },
+      tombfix: {
+        files: {
+          src: 'xpi/components/tombfix.js'
+        },
+        options: {
+          config: '.jscsrc',
+          disallowDanglingUnderscores: null,
+          requireCamelCaseOrUpperCaseIdentifiers: null
+        }
+      },
+      main: {
+        files: {
+          src: [
+            'xpi/chrome/content/overlay/overlay.js',
+            // 'xpi/chrome/content/library/component.js',
+            // 'xpi/chrome/content/library/expand.js',
+            // 'xpi/chrome/content/library/utility.js',
+            // 'xpi/chrome/content/library/tabWatcher.js',
+            'xpi/chrome/content/library/repository.js',
+            // 'xpi/chrome/content/library/models.js',
+            // 'xpi/chrome/content/library/Tombfix.Service.js',
+            'xpi/chrome/content/library/actions.js',
+            // 'xpi/chrome/content/library/extractors.js',
+            // 'xpi/chrome/content/library/ui.js',
+            // 'xpi/chrome/content/quickPostForm.js',
+            // 'xpi/chrome/content/options/options.js',
+            'xpi/chrome/content/changeAccount/changeAccount.js'
+            // 'xpi/chrome/content/wsh.js'
+          ]
+        },
+        options: {
+          config: '.jscsrc',
+          disallowKeywords: ['var', 'continue', 'debugger', 'with']
+        }
+      },
+      old: {
+        files: {
+          src: [
+            'xpi/chrome/content/library/component.js',
+            'xpi/chrome/content/library/utility.js',
+            'xpi/chrome/content/library/tabWatcher.js',
+            'xpi/chrome/content/library/models.js',
+            'xpi/chrome/content/library/Tombfix.Service.js',
+            'xpi/chrome/content/library/extractors.js',
+            'xpi/chrome/content/library/ui.js',
+            'xpi/chrome/content/quickPostForm.js',
+            'xpi/chrome/content/options/options.js'
+          ]
+        },
+        options: {
+          esnext: true,
+          validateIndentation: 2,
+          maximumLineLength: {
+            value: 300,
+            allowUrlComments: true
+          },
+          maximumNumberOfLines: 5000,
+          disallowKeywords: ['debugger', 'with'],
+          disallowImplicitTypeConversion: ['numeric', 'binary'],
+          disallowKeywordsOnNewLine: ['while', 'catch', 'finally'],
+          requireCommaBeforeLineBreak: true,
+          disallowMixedSpacesAndTabs: true,
+          disallowPaddingNewlinesBeforeKeywords: ['else', 'catch', 'finally'],
+          disallowSpacesInCallExpression: true,
+          disallowSpacesInFunctionDeclaration: {
+            beforeOpeningRoundBrace: true
+          },
+          disallowSpacesInNamedFunctionExpression: {
+            beforeOpeningRoundBrace: true
+          },
+          disallowSpacesInsideArrayBrackets: true,
+          disallowTrailingWhitespace: true,
+          requireCapitalizedConstructors: true,
+          requireCurlyBraces: [
+            'else', 'do', 'switch', 'try', 'catch', 'finally'
+          ],
+          requireKeywordsOnNewLine: [
+            'for', 'do', 'switch', 'case', 'default', 'try', 'throw'
+          ],
+          requireOperatorBeforeLineBreak: [
+            '=', '+=', '-=', '*=', '/=', '%=', '<<=', '>>=', '>>>=', '&=', '|=',
+            '^=', '+', '-', '*', '/', '%', '<<', '>>', '>>>', '&', '|', '^',
+            '&&', '||', '==', '===', '>=', '<=', '<', '>', '!=', '!==', '?'
+          ],
+          requirePaddingNewLinesAfterUseStrict: true,
+          requirePaddingNewlinesBeforeKeywords: ['do'],
+          requireParenthesesAroundIIFE: true,
+          requireSpaceAfterKeywords: [
+            'case', 'return', 'throw', 'new', 'instanceof', 'in', 'of', 'yield'
+          ],
+          requireSpaceAfterLineComment: true,
+          requireSpaceBeforeKeywords: ['finally', 'instanceof', 'in', 'of'],
+          requireSpaceBetweenArguments: true,
+          requireSpacesInForStatement: true,
+          validateAlignedFunctionParameters: true
+        }
+      },
+      prefs: {
+        files: {
+          src: 'xpi/defaults/preferences/prefs.js'
+        },
+        options: {
+          config: '.jscsrc',
+          maximumLineLength: null
         }
       }
     },
