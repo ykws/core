bed34b9b4657cc1867e13e2567c29a279bfbed95
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-05-29 19:33:16 +0900

    manual test: Update yahoo_jma_test_patch.js

--- test/manual/yahoo_jma_test_patch.js
+++ test/manual/yahoo_jma_test_patch.js
@@ -1,9 +1,36 @@
-log(Models['Yahoo JMA'].hiraganaToKatakana('aiueo') === 'aiueo');
-log(Models['Yahoo JMA'].hiraganaToKatakana('あいうえお') === 'アイウエオ');
-log(Models['Yahoo JMA'].hiraganaToKatakana('かきくけこ') === 'カキクケコ');
-log(Models['Yahoo JMA'].kanaToRoma('aiueo') === 'aiueo');
-log(Models['Yahoo JMA'].kanaToRoma('あいうえお') === 'aiueo');
-log(Models['Yahoo JMA'].kanaToRoma('かきくけこ') === 'kakikukeko');
-log(Models['Yahoo JMA'].kanaToRoma('アイウエオ') === 'aiueo');
-log(Models['Yahoo JMA'].kanaToRoma('カキクケコ') === 'kakikukeko');
-log(Models['Yahoo JMA'].katakanaTable.get('ア') === 'a');
+{
+  let assert = function assert(...args) {
+    if (assert.count == null) {
+      assert.count = assert.passed = assert.failed = 0;
+    }
+
+    assert.count += 1;
+
+    let [target, val] = args.concat(true);
+
+    if (target === val) {
+      assert.passed += 1;
+    } else {
+      assert.failed += 1;
+
+      console.log(target, val);
+      console.trace();
+    }
+  };
+
+  assert(Models['Yahoo JMA'].hiraganaToKatakana('aiueo'), 'aiueo');
+  assert(Models['Yahoo JMA'].hiraganaToKatakana('あいうえお'), 'アイウエオ');
+  assert(Models['Yahoo JMA'].hiraganaToKatakana('かきくけこ'), 'カキクケコ');
+  assert(Models['Yahoo JMA'].kanaToRoma('aiueo'), 'aiueo');
+  assert(Models['Yahoo JMA'].kanaToRoma('あいうえお'), 'aiueo');
+  assert(Models['Yahoo JMA'].kanaToRoma('かきくけこ'), 'kakikukeko');
+  assert(Models['Yahoo JMA'].kanaToRoma('アイウエオ'), 'aiueo');
+  assert(Models['Yahoo JMA'].kanaToRoma('カキクケコ'), 'kakikukeko');
+  assert(Models['Yahoo JMA'].katakanaTable.get('ア'), 'a');
+
+  console.log([
+    `${createURI(Components.stack.filename).fileName}'s ${assert.count} tests:`,
+    `  * pass: ${assert.passed}`,
+    `  * fail: ${assert.failed}`
+  ].join('\n'));
+}
