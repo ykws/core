a1347a2bfa4bdc5f79a01cd00f16fe6e74c79496
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-07-27 21:53:53 +0900

    Fix "build failing"
    via https://travis-ci.org/tombfix/core/builds/72823569
    
    * Gruntfile.js: Update eslint:old task

--- Gruntfile.js
+++ Gruntfile.js
@@ -259,6 +259,7 @@ module.exports = function createGruntConfig(grunt) {
             'no-shadow': 0,
             'no-undef': 0,
             'no-undefined': 0,
+            'no-unused-vars': 0,
             'no-use-before-define': 0,
 
             'brace-style': 0,
