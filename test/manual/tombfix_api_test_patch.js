b4af4879c5509c5477587e014319dcd27b5f34e8
Syoichi Tsuyuhara <syoichi@outlook.com>
2015-12-29 01:23:26 +0900

    manual test: Update tombfix_api_test_patch.js

--- test/manual/tombfix_api_test_patch.js
+++ test/manual/tombfix_api_test_patch.js
@@ -1,25 +1,68 @@
-log(typeof Tombfix);
-log(typeof Tombloo);
-log(typeof Tombfix.Service);
-log(typeof Tombloo.Service);
-log(typeof Tombfix.Service.extractors);
-log(typeof Tombloo.Service.extractors);
-log(typeof Extractors);
-log(typeof Extractors.ReBlog);
-log(typeof models);
-log(typeof Models);
-log(typeof models.Tumblr);
-log(typeof Models.Tumblr);
-log(typeof Tumblr);
+{
+  let assert = function assert(...args) {
+    if (assert.count == null) {
+      assert.count = assert.passed = assert.failed = 0;
+    }
 
-Tombloo.Service.extractors.register({
-	name : 'Tombloo Path Test',
-	ICON : 'chrome://tombloo/skin/photo.png',
-	check : function check(ctx) {
-		return true;
-	},
-	extract : function extract(ctx) {
-		addTab('chrome://tombloo/content/browser.xul');
-		return {};
-	}
-});
+    assert.count += 1;
+
+    let [target, val] = args.concat(true);
+
+    if (target === val) {
+      assert.passed += 1;
+    } else {
+      assert.failed += 1;
+
+      console.log(target, val);
+      console.trace();
+    }
+  };
+
+  let isObject = function isObject(target) {
+    return typeof target === 'object' && target !== null;
+  };
+
+  assert(isObject(Tombfix));
+  assert(isObject(Tombloo));
+  assert(Tombfix, Tombloo);
+  assert(isObject(Tombfix.Service));
+  assert(isObject(Tombloo.Service));
+  assert(Tombfix.Service, Tombloo.Service);
+  assert(isObject(Extractors));
+  assert(isObject(Extractors.ReBlog));
+  assert(isObject(Tombfix.Service.extractors));
+  assert(isObject(Tombloo.Service.extractors));
+  assert(Extractors, Tombloo.Service.extractors);
+  assert(Extractors instanceof Repository);
+  assert(isObject(Actions));
+  assert(isObject(Actions['----']));
+  assert(isObject(Tombfix.Service.actions));
+  assert(isObject(Tombloo.Service.actions));
+  assert(Actions, Tombloo.Service.actions);
+  assert(Actions instanceof Repository);
+  assert(isObject(Models));
+  assert(isObject(Models.Tumblr));
+  assert(isObject(models));
+  assert(Models, models);
+  assert(Models instanceof Repository);
+  assert(isObject(Tumblr));
+
+  Tombloo.Service.extractors.register({
+    name: 'Tombloo Path Test',
+    ICON: 'chrome://tombloo/skin/photo.png',
+    check(ctx) {
+      return true;
+    },
+    extract(ctx) {
+      addTab('chrome://tombloo/content/overlay/overlay.xul');
+
+      return {};
+    }
+  });
+
+  console.log([
+    `${createURI(Components.stack.filename).fileName}'s ${assert.count} tests:`,
+    `  * pass: ${assert.passed}`,
+    `  * fail: ${assert.failed}`
+  ].join('\n'));
+}
